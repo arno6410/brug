f7d51ccf4cddc53d7755f81b8b6d36b04f7141aa
arno6410 <arno.dehaseleer@gmail.com>
2021-04-21 10:01:23 +0200

    Betere versie van de code
    +Excel schrijven

--- brug.m
+++ brug.m
@@ -1,27 +1,37 @@
-function [krachten] = brug(n)
+function [krachten] = brug(n) %n is aantal tuien per pylon
+
+%% Constanten invoeren
 lengte = 2.2;
 hoogte = 0.5;
 halveafstand = lengte/2;
-afstand = (halveafstand-0.1)/n;
-hoogteverschil = hoogte/n;
+afstand = (halveafstand-0.1)/n; %de afstand tussen 2 tuien op het wegdek
+hoogteverschil = hoogte/n; %hoogteverschil tussen 2 tuien op de pylon
+extrakrachtafstand = (lengte-0.2)/3 + 0.1; %hier grijpt de puntlast aan
+hoek = atan(hoogte/(lengte/2)) * 180 / pi; %dezelfde hoek voor elke tui
 
-links = [];
-rechts = [];
+afstanden = [];
 hoogtes = [];
+krachten = [];
 huidigeafstand = 0.1;
 huidigehoogte = 0;
-extrakrachtafstand = (lengte-0.2)/3 +0.1;
 minimum1 = 5000;
 index1 = 0;
-minimum2 = 5000;
-index2 = 0;
 
-for i=1:n
-    huidigeafstand = huidigeafstand + afstand;
-    huidigehoogte = huidigehoogte + hoogteverschil;
-    links = [links huidigeafstand];
-    rechts = [rechts lengte-huidigeafstand];
-    hoogtes = [hoogtes huidigehoogte];
+va = 400 * afstand;
+N = va / sin(hoek);
+Nh = cos(hoek) * N;
+
+%% Afstanden en krachten berekenen voor elke tui
+for i=1:2*n
+    if(i ~= n+1)
+       huidigeafstand = huidigeafstand + afstand; 
+    end
+    afstanden = [afstanden huidigeafstand];
+    
+    if(mod(i,2) == 0)
+        huidigehoogte = huidigehoogte + hoogteverschil;
+        hoogtes = [hoogtes huidigehoogte];
+    end
     
     if(abs(huidigeafstand-extrakrachtafstand) < minimum1)
         minimum2 = minimum1;
@@ -29,28 +39,26 @@ for i=1:n
         index2 = index1;
         index1 = i;
     end
-end
-hoek = atan(hoogte/(lengte/2)) * 180 / pi;
-
-va = 400 * afstand;
-N = va / sin(hoek);
-Nh = cos(hoek) * N;
-
-krachten = [];
-for i=1:2*n
-    krachtvector = [N;Nh]
+    krachtvector = [afstanden(i);N;Nh];
     krachten = [krachten krachtvector]; 
 end
 
-if (abs(extrakrachtafstand-(links(index1))) < 0.05)
+%% Rekening houden met de puntlast op 66 cm
+%Deze valt juist op een tui als we een veelvoud van 3 gebruiken
+if (abs(extrakrachtafstand-(afstanden(index1))) < 0.05)
     va = 400 * afstand + 200;
     N = va / sin(hoek);
     Nh = cos(hoek) * N;
-    krachtvector = [N;Nh];
+    krachtvector = [afstanden(index1);N;Nh];
     disp(krachten)
     disp(krachtvector)
     krachten(:,index1) = krachtvector;
     disp(krachtvector)
 end
-disp(links)
+
+%% Naar excel schrijven
+titel = int2str(n) + "tuien.xlsx";
+convertStringsToChars(titel)
+disp(titel)
+xlswrite(titel,krachten)
 end
